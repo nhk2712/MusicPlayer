6ee69e9afbe86052ac4fb0ee37fbf80676d7890c
nhk2712 <74997701+nhk2712@users.noreply.github.com>
2021-11-07 11:31:28 +0700

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,3 +1,4 @@
+var container = document.getElementById('container')
 var time = document.getElementById('time')
 var music = document.createElement('audio')
 var play = document.getElementById('play')
@@ -7,86 +8,139 @@ var curtime = document.getElementById('curtime')
 var repeat = document.getElementById('repeat')
 var loopIcon = document.getElementById('loopIcon')
 
-var value=-1
+var title = document.getElementById('title')
+var artist = document.getElementById('artist')
+var artwork = document.getElementById('artwork')
+
+var prev= document.getElementById('prev')
+var next= document.getElementById('next')
+
+var value
 var isLoop = false
-time.onchange = function(){ //Makes the music's current time be the same as the range's value
+
+var song1  = {
+    name:"DotA",
+    dir:"dota",
+    artist:"Basshunter",
+    artwork:"basshunter",
+    color:"lightgreen"
+}
+
+var song2 = {
+    name:"Shivers",
+    dir:"shivers",
+    artist:"Ed Sheeran",
+    artwork:"sheeran",
+    color:"lightcoral"
+}
+
+var list = [song1, song2]
+
+var index = 0
+
+function init() {
+    aud.src="data/song/" + list[index].dir + ".mp3"
+    curtime.innerText = "0 : 00"
+    value = -1
+    title.innerText = list[index].name
+    artist.innerText = list[index].artist
+    artwork.src = "data/artwork/" + list[index].artwork + ".jpg"
+    container.style.backgroundColor = list[index].color
+}
+
+time.onchange = function () { //Makes the music's current time be the same as the range's value
     aud.currentTime = time.value
-    curtime.innerText=secToMinStr(aud.currentTime)
-    value=aud.currentTime
+    curtime.innerText = secToMinStr(aud.currentTime)
+    value = aud.currentTime
 }
 
 //Initially, the audio is considered to be paused
-play.addEventListener('click',firstPlayFunc)
-function firstPlayFunc(){ //Plays for the first time (and occurs during the playback as well)
+play.addEventListener('click', firstPlayFunc)
+function firstPlayFunc() { //Plays for the first time (and occurs during the playback as well)
     if (aud.paused) bar() //When the audio is paused and the user wants to play it
     else unbar() //When the audio is playing and the user wants to pause it
 }
 
-music.setAttribute("src","data/song/song.mp3")
-music.setAttribute("id","aud")
+music.setAttribute("src", "data/song/" + list[index].dir + ".mp3")
+music.setAttribute("id", "aud")
 document.body.appendChild(music)
 
 var aud = document.getElementById('aud')
-aud.volume=0.3
+aud.volume = 0.3
+init()
 
 var inter
-curtime.innerText="0 : 00"
-function bar(){ //Pause to play
-    inter = setInterval(function(){ //Makes the range and string indicator be consistent to the audio playback
+function bar() { //Pause to play
+    inter = setInterval(function () { //Makes the range and string indicator be consistent to the audio playback
         value++
-        time.value=value;
-        curtime.innerText=secToMinStr(aud.currentTime)
-    },1000)
+        time.value = value;
+        curtime.innerText = secToMinStr(aud.currentTime)
+    }, 1000)
     aud.play()
-    play.src="icon/pause.png"
+    play.src = "icon/pause.png"
 }
 
-function unbar(){ //Play to pause
+function unbar() { //Play to pause
     clearInterval(inter)
     aud.pause()
-    play.src="icon/play.png"
+    play.src = "icon/play.png"
 }
 
-volume.onmousemove = function(){ //Change the audio's volume
-    aud.volume=volume.value/100
+volume.onmousemove = function () { //Change the audio's volume
+    aud.volume = volume.value / 100
 }
 
-aud.onloadedmetadata= function(){
-    time.max=this.duration
-    maxtime.innerText=secToMinStr(aud.duration)
+aud.onloadedmetadata = function () {
+    time.max = this.duration
+    maxtime.innerText = secToMinStr(aud.duration)
 }
 
-function secToMinStr(sec){
-    var min=Math.floor(sec/60)
-    var minStr=min.toString()
-    var secDis=Math.floor(sec%60)
-    var secStr=secDis.toString()
-    if (secDis<10) return minStr+" : 0"+secStr
-    else return minStr+" : "+secStr
+function secToMinStr(sec) {
+    var min = Math.floor(sec / 60)
+    var minStr = min.toString()
+    var secDis = Math.floor(sec % 60)
+    var secStr = secDis.toString()
+    if (secDis < 10) return minStr + " : 0" + secStr
+    else return minStr + " : " + secStr
 }
 
-aud.onended=function(){
+aud.onended = function () {
     unbar()
-    play.removeEventListener('click',firstPlayFunc)
-    play.addEventListener('click',again)
+    play.removeEventListener('click', firstPlayFunc)
+    play.addEventListener('click', again)
     if (isLoop) play.click()
+    else if (index!=list.length-1) next.click()
 }
 
-function again(){
-    value=0
-    time.value=0
-    play.removeEventListener('click',again)
-    play.addEventListener('click',firstPlayFunc)
+function again() {
+    value = 0
+    time.value = 0
+    play.removeEventListener('click', again)
+    play.addEventListener('click', firstPlayFunc)
     bar()
 }
 
-repeat.onclick = function(){
-    if (!isLoop){
-        isLoop=true;
-        loopIcon.style.color="DodgerBlue"
+repeat.onclick = function () {
+    if (!isLoop) {
+        isLoop = true;
+        loopIcon.style.color = "DodgerBlue"
     }
-    else{
-        isLoop=false
-        loopIcon.style.color="white"
+    else {
+        isLoop = false
+        loopIcon.style.color = "white"
     }
+}
+
+prev.onclick = function () {
+    if (index==0) index=list.length-1
+    else index--
+    init()
+    play.click()
+}
+
+next.onclick = function () {
+    if (index==list.length-1) index=0
+    else index++
+    init()
+    play.click()
 }
